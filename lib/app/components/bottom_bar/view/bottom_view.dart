96ac98e817e397f304f36b6aea365ac3e91b8ae6
Hasan Alper Duzgun <alper-mf@hotmail.com>
2022-05-19 00:08:03 +0300

    Bottom Appbar Is Updated

--- lib/app/components/bottom_bar/view/bottom_view.dart
+++ lib/app/components/bottom_bar/view/bottom_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:nft_tool_app/app/components/bottom_bar/controller/bottom_controller.dart';
 import 'package:nft_tool_app/app/constants/assets_const.dart';
+import 'package:nft_tool_app/app/constants/colors.dart';
 import 'package:nft_tool_app/app/constants/padding_const.dart';
 import 'package:nft_tool_app/app/init/size_config.dart';
 
@@ -18,55 +19,65 @@ class BottomAppbarView extends GetView<BottomAppbarController> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Obx(
-            () => Container(
-              padding: const EdgeInsets.fromLTRB(globalPadding + 5, 0, globalPadding + 5, 0),
-              height: SizeConfig.height * .1,
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () => controller.setPageIndex(0),
-                    child: SvgPicture.asset(
-                      exploreIcon,
-                      color: controller.page.value == 0
-                          ? Colors.white.withOpacity(0.8)
-                          : Colors.grey.withOpacity(0.5),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () => controller.setPageIndex(1),
-                    child: SvgPicture.asset(
-                      statsIcon,
-                      color: controller.page.value == 1
-                          ? Colors.white.withOpacity(0.8)
-                          : Colors.grey.withOpacity(0.5),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () => controller.setPageIndex(2),
-                    child: SvgPicture.asset(
-                      prizeIcon,
-                      color: controller.page.value == 2
-                          ? Colors.white.withOpacity(0.8)
-                          : Colors.grey.withOpacity(0.5),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () => controller.setPageIndex(3),
-                    child: SvgPicture.asset(
-                      walletIconMini,
-                      color: controller.page.value == 3
-                          ? Colors.white.withOpacity(0.8)
-                          : Colors.grey.withOpacity(0.5),
-                    ),
-                  ),
-                ],
-              ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(globalPadding + 5, 0, globalPadding + 5, 0),
+            height: SizeConfig.height * .1,
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: () => controller.setPageIndex(0),
+                  child: const _BottomButtonWidget(icon: exploreIcon, index: 0),
+                ),
+                InkWell(
+                  onTap: () => controller.setPageIndex(1),
+                  child: const _BottomButtonWidget(icon: statsIcon, index: 1),
+                ),
+                InkWell(
+                  onTap: () => controller.setPageIndex(2),
+                  child: const _BottomButtonWidget(icon: prizeIcon, index: 2),
+                ),
+                InkWell(
+                  onTap: () => controller.setPageIndex(3),
+                  child: const _BottomButtonWidget(icon: walletIconMini, index: 3),
+                ),
+              ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _BottomButtonWidget extends GetView<BottomAppbarController> {
+  final String icon;
+  final int index;
+  const _BottomButtonWidget({
+    Key? key,
+    required this.icon,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Container(
+        height: 24,
+        width: 24,
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              color:
+                  controller.page.value == index ? greenColor.withOpacity(0.5) : Colors.transparent,
+              blurRadius: 24)
+        ]),
+        child: SvgPicture.asset(
+          icon,
+          color: controller.page.value == index
+              ? greenColor.withOpacity(0.6)
+              : Colors.grey.withOpacity(0.5),
+        ),
       ),
     );
   }
