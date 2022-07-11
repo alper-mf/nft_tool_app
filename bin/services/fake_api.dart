f9bbcb25f52bded316b0654b4c0671ea1516698c
Hasan Alper Duzgun <alper-mf@hotmail.com>
2022-07-12 02:52:42 +0300

    NFT1- Error Message Is Added

--- bin/services/fake_api.dart
+++ bin/services/fake_api.dart
@@ -4,13 +4,20 @@ import 'package:path/path.dart' as path;
 import 'api_models.dart';
 
 class FakeApi implements ApiModels {
+  String errorMessage(String val) {
+    return '''{
+        "title": "Error",
+        "message": $val
+      }''';
+  }
+
   @override
   Future<String> getString(String url) async {
     final targetFile = File(path.join(path.dirname(Platform.script.toFilePath()), url));
     try {
       return await targetFile.readAsString();
     } catch (e) {
-      return '$e';
+      return errorMessage(e.toString());
     }
   }
 }
